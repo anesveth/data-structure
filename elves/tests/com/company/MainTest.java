7bc2c4fd07cafaaba75bbf5e62bceb7f8c30f76e
anesveth <annesveth@ufm.edu>
2020-01-15 12:43:58 -0600

    f

---
+++ elves/tests/com/company/MainTest.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertEquals(2,Main.Fuel(12));
+    }
+}
