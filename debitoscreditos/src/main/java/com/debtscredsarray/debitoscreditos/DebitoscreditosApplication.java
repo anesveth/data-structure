e74efe1d6ac19c66e2388ee7d3c07fac9b851228
anesveth <annesveth@ufm.edu>
2020-01-31 23:33:42 -0600

    Initialized spring project for credit/debit hw

---
+++ debitoscreditos/src/main/java/com/debtscredsarray/debitoscreditos/DebitoscreditosApplication.java
@@ -0,0 +1,13 @@
+package com.debtscredsarray.debitoscreditos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitoscreditosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebitoscreditosApplication.class, args);
+	}
+
+}
---
+++ debitoscreditos/src/test/java/com/debtscredsarray/debitoscreditos/DebitoscreditosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debtscredsarray.debitoscreditos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitoscreditosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
