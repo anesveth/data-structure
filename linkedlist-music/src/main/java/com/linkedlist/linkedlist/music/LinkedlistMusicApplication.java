7d79cdbb9ae86df0e2f77a6dae98ab455db6baa7
anesveth <annesveth@ufm.edu>
2020-02-19 08:56:07 -0600

    spring for linkedlists ex

---
+++ linkedlist-music/src/main/java/com/linkedlist/linkedlist/music/LinkedlistMusicApplication.java
@@ -0,0 +1,13 @@
+package com.linkedlist.linkedlist.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedlistMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedlistMusicApplication.class, args);
+	}
+
+}
---
+++ linkedlist-music/src/test/java/com/linkedlist/linkedlist/music/LinkedlistMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedlist.linkedlist.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedlistMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
