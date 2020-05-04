574b02ed2e4fabac0085e6dc4290b6754a4c7bcd
anesveth <annesveth@ufm.edu>
2020-05-03 23:42:12 -0600

    benchmarking document

--- lab_27_abril (1)/searching_benchmark.py
+++ lab_27_abril (1)/searching_benchmark.py
@@ -1,16 +1,15 @@
 from random import randint
 from searching import linear_search, jump_search, binary_search
 
-sorted_list = list(range(1, 5000))
-search_value = randint(1, 4999)
+sorted_list = list(range(1, 1000))
+search_value = randint(1, 999)
 
 def test_linear_search(benchmark):
     benchmark(linear_search, sorted_list, search_value)
 
 def test_jump_search(benchmark):
-    benchmark(jump_search, sorted_list, search_value, 10)
+    benchmark(jump_search, sorted_list, search_value, 50)
 
 def test_binary_search(benchmark):
     benchmark(binary_search,sorted_list,search_value)
-print(linear_search(sorted_list,search_value))
-print(binary_search(sorted_list,search_value))
+
